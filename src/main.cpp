a02ec939aea6395c28e628e8a5b70bbdebcc1d6b
Neal Terrell <nealdt@gmail.com>
2026-01-22 11:53:42 -0800

    Use glm from day 1.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <array>
-
+#include <glm/ext.hpp>
 /*
 This program is a template for an SFML project using CMake and Vcpkg.
 You can duplicate the folder containing this project to easily set up a new
@@ -9,7 +9,7 @@ SFML program.
 Just delete the "out" folder in the new project , or else you might get CMake errors.
 */
 
-void drawPixel(sf::RenderWindow& window, sf::Vector2i position, sf::Color color) {
+void drawPixel(sf::RenderWindow& window, glm::ivec2 position, sf::Color color) {
 	float pX{ static_cast<float>(position.x) };
 	float pY{ static_cast<float>(position.y) };
 	sf::Vertex pixel[1] {
@@ -44,7 +44,7 @@ int main() {
 		*/
 		for (int32_t x{ 0 }; x < 500; ++x) {
 			int32_t y{ static_cast<int32_t>(std::round(2.0 / 3.0 * x + 40)) };
-			drawPixel(window, sf::Vector2i{ x, y }, sf::Color::White);
+			drawPixel(window, glm::ivec2{ x, y }, sf::Color::White);
 		}
 	}
 
